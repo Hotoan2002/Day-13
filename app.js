7f7be575eecf89c6e37b9f43b459ba8f4ef36142
toan <toanzzz2k2@gmail.com>
2022-05-20 18:31:23 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var upload = document.querySelector('#mypicture')
+var preview = document.querySelector('.preview')
+var error = document.querySelector('.error')
+
+
+upload.addEventListener('change', function(e){
+
+    var file = upload.files[0]
+
+    if(!file)
+        return 
+
+    if(!file.name.endsWith('.jpg')){
+        error.innerHTML = `Hinh anh phai thuoc dang jpeg`
+        return
+    }else{
+        error.innerHTML = ``
+    }
+    
+     if(file.size / (1024 * 1024) > 5){
+        error.innerHTML = `chi duoc upload anh <5MB`
+        return
+    }else{
+        error.innerHTML = ``
+    }
+    console.log(file);
+
+    var img = document.createElement('img')
+    img.src = URL.createObjectURL(file)
+    preview.appendChild(img)
+})
